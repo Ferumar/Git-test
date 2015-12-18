a1705744a2620c142c07014a97b9ec2d2084abf8
Ferumar <romanyuki@ukr.net>
2015-12-18 18:56:07 +0200

    A+B

--- Git-test-project/src/ua/kk/test/Main.java
+++ Git-test-project/src/ua/kk/test/Main.java
@@ -2,7 +2,10 @@ package ua.kk.test;
 
 public class Main {
 	public static void main(String[] args){
-	
+		int a = 23;
+		int b = 26;
+		
 		System.out.println("Hello from Git hub!!!!1");
+		System.out.println(a+b);
 	}
 }
